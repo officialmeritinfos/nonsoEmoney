24aac10a4dde51439dd04d06d85b456c3b472961
Anon Dev <michaelerastus9@gmail.com>
2024-09-19 11:04:16 +0100

    Addedtradechart

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -23,7 +23,66 @@
 
             <div class="col-lg-6 col-md-12">
                 <div class="main-banner-image">
-                    <img src="{{asset('home/img/banner/banner1.png')}}" alt="image">
+                    <!-- TradingView Widget BEGIN -->
+                    <div class="tradingview-widget-container">
+                        <div class="tradingview-widget-container__widget"></div>
+                        <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/"
+                                rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on
+                                    TradingView</span></a></div>
+                        <script type="text/javascript"
+                            src="https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js" async>
+                            {
+    "symbols": [
+      [
+        "Apple",
+        "AAPL|1D"
+      ],
+      [
+        "Google",
+        "GOOGL|1D"
+      ],
+      [
+        "Microsoft",
+        "MSFT|1D"
+      ]
+    ],
+    "chartOnly": false,
+    "width": "100%",
+    "height": "500",
+    "locale": "en",
+    "colorTheme": "light",
+    "autosize": true,
+    "showVolume": false,
+    "showMA": false,
+    "hideDateRanges": false,
+    "hideMarketStatus": false,
+    "hideSymbolLogo": false,
+    "scalePosition": "right",
+    "scaleMode": "Normal",
+    "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+    "fontSize": "10",
+    "noTimeScale": false,
+    "valuesTracking": "1",
+    "changeMode": "price-and-percent",
+    "chartType": "area",
+    "maLineColor": "#2962FF",
+    "maLineWidth": 1,
+    "maLength": 9,
+    "headerFontSize": "medium",
+    "lineWidth": 2,
+    "lineType": 0,
+    "dateRanges": [
+      "1d|1",
+      "1m|30",
+      "3m|60",
+      "12m|1D",
+      "60m|1W",
+      "all|1M"
+    ]
+  }
+                        </script>
+                    </div>
+                    <!-- TradingView Widget END -->
                 </div>
             </div>
         </div>
